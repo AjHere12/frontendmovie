6e68653e691803d016dd53c4c80025689015e8d7
Arun Jyoti Sahoo <akhilbandhu42096@gmail.com>
2024-03-24 22:56:24 +0530

    signup login page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import  { HomePages } from "./home/HomePages"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Header from "./components/header/Header"
 import {Footer} from "./components/footer/Footer"
+import LoginSignup from "./components/LoginSignup"
+import SinglePage from "./components/watch/SinglePage"
 function App() {
   return (
     <>
@@ -11,7 +13,8 @@ function App() {
         
         <Switch>  
           <Route exact path='/' component={HomePages} />
-          {/*<Route path='/singlepage/:id' component={SinglePage} exact />*/}
+          <Route path='/singlePage/:id' component={SinglePage} exact />
+          <Route path='/login' component={LoginSignup} />
         </Switch>
         <Footer />
       </Router>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -39,7 +39,10 @@ const Header = () => {
           <div className='account flexSB'>
             <i className='fa fa-search'></i>
             <i class='fas fa-bell'></i>
+
+       
             <i className='fas fa-user'></i>
+           
             <button>Subscribe Now</button> 
           </div>
         </div>
